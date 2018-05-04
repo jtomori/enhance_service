91352300bca2dff1cd4ee5af3d255bfa32bcd6ba
jtomori <tomori.juraj@gmail.com>
2018-05-04 10:04:51 +0200

    doing initial tests with flask

---
+++ service.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/<id>', methods=['GET',])
+def printId(id):
+    return id
+
+if __name__ == '__main__':
+    app.run()
